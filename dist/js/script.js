c17e1981692473273729f056860626c92d0acfec
Muharromi Putra Ardhana <muharromi42@gmail.com>
2023-04-14 14:54:43 +0700

    navbar transparent

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,15 @@
+// navbar fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixednav = header.offsetTop;
+
+    if (window.pageYOffset > fixednav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+};
+
 //hamburger
 const hamburger = document.querySelector('#hamburger');
 
